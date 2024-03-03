3da7272922cefa8917d14051960235ade8211074
mpe02 <mpe02@192.168.1.2>
2024-03-03 14:46:21 -0300

    teste sincronizando com git2!

---
+++ tcprog/src/main/java/com/mycompany/tcprog/Tcprog.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tcprog;
+
+/**
+ *
+ * @author mpe02
+ */
+public class Tcprog {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Mundo!");
+    }
+}
